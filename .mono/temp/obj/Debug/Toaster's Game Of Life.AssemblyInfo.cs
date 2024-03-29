21eaa96f6fed4fb7b66228563dad217d3d9f1144
crimeraaa <118309721+Crimeraaa-S1GMA@users.noreply.github.com>
2024-03-29 22:35:41 +0100

    ...

--- .mono/temp/obj/Debug/Toaster's Game Of Life.AssemblyInfo.cs
+++ .mono/temp/obj/Debug/Toaster's Game Of Life.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Toaster\'s Game Of Life")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd1c05a317cf902dbdbc8e27f5c03ea542c97979")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dc2c4deb58edb30b091094d76318c7b2c16a193")]
 [assembly: System.Reflection.AssemblyProductAttribute("Toaster\'s Game Of Life")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Toaster\'s Game Of Life")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
